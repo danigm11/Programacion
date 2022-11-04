165fcb4e674ad6c0a754eb13944cc511e6be6ea8
danigm11 <81052893+danigm11@users.noreply.github.com>
2022-11-04 16:23:09 +0100

    Add files via upload

---
+++ Ej4.java
@@ -0,0 +1,21 @@
+/*
+ * Ej4.java
+ * 
+ * Daniel García Mesa 4/11/2022
+ * 
+ * Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+ * convertir deberá estar almacenada en una variable.
+ * 
+ */
+
+
+public class Ej4 {
+	
+	public static void main (String[] args) {
+		double numeroEuros=100;
+		double valorConver=0.006;
+		System.out.println(numeroEuros+" euros en pesetas es: "+(numeroEuros/valorConver));
+		
+	}
+}
+
