fd1e1c6505f887c9a505707bb7bbe773085595b6
danigm11 <81052893+danigm11@users.noreply.github.com>
2022-11-04 16:35:07 +0100

    Add files via upload

---
+++ Ej1.java
@@ -0,0 +1,27 @@
+/*
+ * Ej1.java
+ * 
+ * Daniel García Mesa 4/11/2022
+ * 
+ * Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+ * los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+ * el valor de cada variable, la suma, la resta, la división y la multiplicación.
+ * 
+ */
+
+
+public class Ej1 {
+	
+	public static void main (String[] args) {
+		double x=144;
+		double y=999;
+		System.out.println("El valor de X es: "+x);
+		System.out.println("El valor de Y es: "+y);
+		System.out.println("La suma de X e Y es: "+(x+y));
+		System.out.println("La resta de X e Y es: "+(x-y));
+		System.out.println("La multiplicacion de X e Y es: "+(x*y));
+		System.out.println("La suma de X e Y es: "+(x+y));
+		
+	}
+}
+
