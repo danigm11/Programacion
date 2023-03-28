70699c50674cc3c0501559f40579bef9090da085
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:03:46 +0200

    Update Canario.java

--- Ej3/Animales/Canario.java
+++ Ej3/Animales/Canario.java
@@ -1,17 +1,25 @@
 package Ej3.Animales;
-
+/*
+ * Canario.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase canario, hereda de Ave
+ * 
+ */
 public class Canario extends Ave {
-  public Canario() {}
+  public Canario() {
+  }
   public Canario(Sexo s) {
-  super(s);
+    super(s);
   }
   public void canta() {
-  System.out.println("Tralaralariiiiiiii");
+    System.out.println("Tralaralariiiiiiii");
   }
   public void caza() {
-  System.out.println("Los canarios no cazan");
+    System.out.println("Los canarios no cazan");
   }
   public void pia() {
     System.out.println("Pio pio pio");
-    }
-    }
+  }
+}
