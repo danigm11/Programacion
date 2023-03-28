529ed5926a739844f72fb517458707db5d01cfa8
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:26:37 +0200

    Update Terminal.java

--- Ej8y9/Terminales/Terminal.java
+++ Ej8y9/Terminales/Terminal.java
@@ -1,30 +1,38 @@
 package Ej8y9.Terminales;
+/*
+ * Terminal.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase terminal, contiene el numero, el tiempo de conversación y una función para llamar
+ * 
+ */
 public class Terminal {
   private String numero;
   private int tiempoDeConversacion; // tiempo de conversación en segundos
   public Terminal(String numero) {
-  this.numero = numero;
-  this.tiempoDeConversacion = 0;
+    this.numero = numero;
+    this.tiempoDeConversacion = 0;
   }
   public String getNumero() {
-  return numero;
+    return numero;
   }
   public void setNumero(String numero) {
-  this.numero = numero;
+    this.numero = numero;
   }
   public int getTiempoDeConversacion() {
-  return tiempoDeConversacion;
+    return tiempoDeConversacion;
   }
   public void setTiempoDeConversacion(int tiempoDeConversacion) {
-  this.tiempoDeConversacion = tiempoDeConversacion;
+    this.tiempoDeConversacion = tiempoDeConversacion;
   }
   @Override
   public String toString() {
-  return "Nº " + numero + " - " + this.tiempoDeConversacion + "s de conversación ";
+    return "Nº " + numero + " - " + this.tiempoDeConversacion + "s de conversación ";
   }
   public void llama(Terminal t, int segundosDeLlamada) {
-  this.tiempoDeConversacion += segundosDeLlamada;
-  t.tiempoDeConversacion += segundosDeLlamada;
+    this.tiempoDeConversacion += segundosDeLlamada;
+    t.tiempoDeConversacion += segundosDeLlamada;
   }
-  }
+ }
   
