73ec966b6b212453bb9946bc26fc8e5a5c2ef12a
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:10:37 +0200

    Update Sexo.java

--- Ej3/Animales/Sexo.java
+++ Ej3/Animales/Sexo.java
@@ -1,4 +1,12 @@
 package Ej3.Animales;
+/*
+ * Sexo.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Tipo enumerado utilizado para definir el sexo de los animales
+ * 
+ */
 public enum Sexo {
   MACHO, HEMBRA
 }
