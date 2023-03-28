f03fb698bfe7aaf5723ea631d2bb5b5c83b00062
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:13:34 +0200

    Update Pizza.java

--- Ej5/Pizza.java
+++ Ej5/Pizza.java
@@ -1,4 +1,12 @@
 package Ej5;
+/*
+ * Pizza.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase pizza contiene datos de las pizzas y una función para servirlas
+ * 
+ */
 public class Pizza {
   private static int totalPedidas = 0;
   private static int totalServidas = 0;
@@ -6,27 +14,27 @@ public class Pizza {
   private String tipo;
   private String estado;
   public Pizza(String tipo, String tamano) {
-  this.tipo = tipo;
-  this.tamano = tamano;
-  this.estado = "pedida";
-  Pizza.totalPedidas++;
+    this.tipo = tipo;
+    this.tamano = tamano;
+    this.estado = "pedida";
+    Pizza.totalPedidas++;
   }
   public String toString() {
-  return "pizza " + this.tipo + " " + this.tamano + ", " + this.estado;
+    return "pizza " + this.tipo + " " + this.tamano + ", " + this.estado;
   }
   public static int getTotalPedidas() {
-  return Pizza.totalPedidas;
+    return Pizza.totalPedidas;
   }
   public static int getTotalServidas() {
-  return Pizza.totalServidas;
+    return Pizza.totalServidas;
   }
   public void sirve() {
     if (this.estado.equals("pedida")) {
-    this.estado = "servida";
-    Pizza.totalServidas++;
+      this.estado = "servida";
+      Pizza.totalServidas++;
     } else {
-    System.out.println("esa pizza ya se ha servido");
+        System.out.println("esa pizza ya se ha servido");
     }
-    }
-    }
+  }
+ }
       
