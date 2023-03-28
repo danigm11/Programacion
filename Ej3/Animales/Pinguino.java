485663bbf5c8750bdfff4c10a0878e59aabbec16
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:09:56 +0200

    Update Pinguino.java

--- Ej3/Animales/Pinguino.java
+++ Ej3/Animales/Pinguino.java
@@ -1,18 +1,26 @@
 package Ej3.Animales;
-
+/*
+ * Pinguino.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase pinguino, hereda de Ave
+ * 
+ */
 public class Pinguino extends Ave {
-  public Pinguino() {}
+  public Pinguino() {
+  }
   public Pinguino(Sexo s) {
-  super(s);
+    super(s);
   }
   public void vuela() {
-  System.out.println("Soy un pingüino, no puedo volar");
+    System.out.println("Soy un pingüino, no puedo volar");
   }
   public void programa() {
-  System.out.println("Soy un pingüino programador, estoy programando en Java");
+    System.out.println("Soy un pingüino programador, estoy programando en Java");
   }
   public void nada() {
-  System.out.println("Estoy nadando");
+    System.out.println("Estoy nadando");
   }
-  }
+ }
   
