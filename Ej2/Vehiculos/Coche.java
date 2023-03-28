62931cc2f7c21be72360a51163663ad6356baa57
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 15:56:22 +0200

    Update Coche.java

--- Ej2/Vehiculos/Coche.java
+++ Ej2/Vehiculos/Coche.java
@@ -1,12 +1,20 @@
 package Ej2.Vehiculos;
-
+/*
+ * Coche.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase coche, clase que hereda de vehículo y tiene una función para encender las luces
+ * 
+ * 
+ */
 public class Coche extends Vehiculo{
   
   public Coche() {
     super();
     }
     
-    public void derrapa() {
-      System.out.println("Hace un derrape");
+    public void luces() {
+      System.out.println("Enciende las luces");
     }
 }
