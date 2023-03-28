ffa0a66cf8384b3c478aa2bd29f489222f03703e
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:27:33 +0200

    Update PruebaTerminales.java

--- Ej8y9/PruebaTerminales.java
+++ Ej8y9/PruebaTerminales.java
@@ -1,21 +1,28 @@
 package Ej8y9;
 
 import Ej8y9.Terminales.Terminal;
-
+/*
+ * PruebaTerminales.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Prueba la clase terminales
+ * 
+ */
 public class PruebaTerminales {
   public static void main(String[] args) {
-  Terminal t1 = new Terminal("678 11 22 33");
-  Terminal t2 = new Terminal("644 74 44 69");
-  Terminal t3 = new Terminal("622 32 89 09");
-  Terminal t4 = new Terminal("664 73 98 18");
-  System.out.println(t1);
-  System.out.println(t2);
-  t1.llama(t2, 320);
-  t1.llama(t3, 200);
-  System.out.println(t1);
-  System.out.println(t2);
-  System.out.println(t3);
-  System.out.println(t4);
+    Terminal t1 = new Terminal("678 11 22 33");
+    Terminal t2 = new Terminal("644 74 44 69");
+    Terminal t3 = new Terminal("622 32 89 09");
+    Terminal t4 = new Terminal("664 73 98 18");
+    System.out.println(t1);
+    System.out.println(t2);
+    t1.llama(t2, 320);
+    t1.llama(t3, 200);
+    System.out.println(t1);
+    System.out.println(t2);
+    System.out.println(t3);
+    System.out.println(t4);
   }
-  }
+ }
   
