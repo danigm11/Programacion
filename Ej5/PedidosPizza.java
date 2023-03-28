04ff09ede9e87b48e1e55fb3811af4dec49914a6
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:14:33 +0200

    Update PedidosPizza.java

--- Ej5/PedidosPizza.java
+++ Ej5/PedidosPizza.java
@@ -1,15 +1,23 @@
 package Ej5;
+/*
+ * PedidosPizza.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Prueba la clase pizza
+ * 
+ */
 public class PedidosPizza {
   public static void main(String[] args) {
-  Pizza p1 = new Pizza("margarita", "mediana");
-  Pizza p2 = new Pizza("funghi", "familiar");
-  p2.sirve();
-  Pizza p3 = new Pizza("cuatro quesos", "mediana");
-  System.out.println(p1);
-  System.out.println(p2);
-  System.out.println(p3);
-  p2.sirve();
-  System.out.println("pedidas: " + Pizza.getTotalPedidas());
-  System.out.println("servidas: " + Pizza.getTotalServidas());
-  }
+    Pizza p1 = new Pizza("margarita", "mediana");
+    Pizza p2 = new Pizza("funghi", "familiar");
+    p2.sirve();
+    Pizza p3 = new Pizza("cuatro quesos", "mediana");
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println(p3);
+    p2.sirve();
+    System.out.println("pedidas: " + Pizza.getTotalPedidas());
+    System.out.println("servidas: " + Pizza.getTotalServidas());
+   }
   }
