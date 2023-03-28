601e4d8e2ea4ffa9479c948f7f6b2c2434b109d8
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:20:46 +0200

    Update Zona.java

--- Ej7/Zona.java
+++ Ej7/Zona.java
@@ -1,11 +1,19 @@
 package Ej7;
+/*
+ * Zona.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase zona contiene las entradas no vendidas aún y tiene funciones para la venta
+ * 
+ */
 public class Zona {
   private int entradasPorVender;
   public Zona(int n){
-  entradasPorVender = n;
+    entradasPorVender = n;
   }
   public int getEntradasPorVender() {
-  return entradasPorVender;
+    return entradasPorVender;
   }
   /**
   * Vende un número de entradas.
@@ -14,16 +22,15 @@ public class Zona {
   * @param n número de entradas a vender
   */
   public void vender(int n) {
-  if (this.entradasPorVender == 0) {
-  System.out.println("Lo siento, las entradas para esa zona están agotadas.");
-  } else if (this.entradasPorVender < n) {
-  System.out.println("Sólo me quedan " + this.entradasPorVender
-  + " entradas para esa zona.");
+    if (this.entradasPorVender == 0) {
+      System.out.println("Lo siento, las entradas para esa zona están agotadas.");
+    } else if (this.entradasPorVender < n) {
+      System.out.println("Sólo me quedan " + this.entradasPorVender+ " entradas para esa zona.");
+    }
+    if (this.entradasPorVender >= n) {
+      entradasPorVender -= n;
+      System.out.println("Aquí tiene sus " + n + " entradas, gracias.");
+    }
   }
-  if (this.entradasPorVender >= n) {
-  entradasPorVender -= n;
-  System.out.println("Aquí tiene sus " + n + " entradas, gracias.");
-  }
-  }
-  }
+ }
   
