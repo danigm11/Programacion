3350175cdaef196aeb1fd0281e7007262d129ad7
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 02:50:21 +0200

    Update Helicoptero.java

--- Ejercicios3/Ej6/Vehiculos/Helicoptero.java
+++ Ejercicios3/Ej6/Vehiculos/Helicoptero.java
@@ -1,5 +1,12 @@
 package Ej6.Vehiculos;
-
+/*
+ * Helicoptero.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * 23/03/2023
+ * 
+ */
 public class Helicoptero extends Aereo{
   private int numHelices;
   public int getNumHelices() {
