4d3a65143bd1e4797e1ca2aceb85fe253d7c3669
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-03-28 16:07:31 +0200

    Update Lagarto.java

--- Ej3/Animales/Lagarto.java
+++ Ej3/Animales/Lagarto.java
@@ -1,18 +1,26 @@
 package Ej3.Animales;
-
+/*
+ * Lagarto.java
+ * 
+ * @author Daniel García Mesa 
+ * 
+ * Clase Lagarto, hereda de Animal
+ * 
+ */
 public class Lagarto extends Animal {
-  public Lagarto() {}
+  public Lagarto() {
+  }
   public Lagarto(Sexo s) {
-  super(s);
+    super(s);
   }
   public void tomaElSol() {
-  System.out.println("Estoy tomando el Sol");
+    System.out.println("Estoy tomando el Sol");
   }
   public void trepa() {
-  System.out.println("Trepa un árbol");
+    System.out.println("Trepa un árbol");
   }
   public void escondete() {
-  System.out.println("Me he escondido, ya no me puedes ver");
+    System.out.println("Me he escondido, ya no me puedes ver");
   }
-  }
+ }
   
