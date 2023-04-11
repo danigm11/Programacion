2b17906421029ab8f2e4da53d29b674cd5496977
danigm11 <81052893+danigm11@users.noreply.github.com>
2023-04-11 16:56:33 +0200

    Add files via upload

---
+++ Ej2/Vehiculos/Bicicleta.java
@@ -0,0 +1,13 @@
+package Ej2.Vehiculos;
+
+public class Bicicleta extends Vehiculo{
+  
+  public Bicicleta() {
+    super();
+    }
+    
+    public void caballito() {
+      System.out.println("Hace el caballito");
+    }
+}
+
